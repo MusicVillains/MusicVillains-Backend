bf29550bae95a74c37fdde8afd023f3e4b559b10
woody35545 <woody35545@gmail.com>
2023-08-06 13:51:43 +0900

    [+] Initialize project

---
+++ src/main/java/com/teamseven/MusicVillain/MusicVillainApplication.java
@@ -0,0 +1,13 @@
+package com.teamseven.MusicVillain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicVillainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicVillainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamseven/MusicVillain/MusicVillainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamseven.MusicVillain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicVillainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
