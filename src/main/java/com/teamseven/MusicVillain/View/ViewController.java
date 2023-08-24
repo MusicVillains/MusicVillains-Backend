698eeae81d388931ec2fa1bf56eb75179e159080
woody35545 <woody35545@gmail.com>
2023-08-25 04:17:37 +0900

    [+] Add Logback setting and banner

--- src/main/java/com/teamseven/MusicVillain/View/ViewController.java
+++ src/main/java/com/teamseven/MusicVillain/View/ViewController.java
@@ -2,6 +2,9 @@ package com.teamseven.MusicVillain.View;
 
 import com.teamseven.MusicVillain.Member.MemberController;
 import com.teamseven.MusicVillain.Member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +12,9 @@ import org.springframework.web.bind.annotation.*;
 
 
 @Controller
+//@Slf4j
 public class ViewController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private MemberController memberController;
     private final MemberService memberService;
@@ -22,6 +27,10 @@ public class ViewController {
 
     @GetMapping({"/", "/index"})
     public String index(){
+        log.info("index.html");
+        log.debug("index.html");
+        log.trace("index.html");
+        log.error("index.html");
         return "index"; // returns index.html
     }
 
