4325e5e5431afffcf165d3463c4c007055b2b05d
woody35545 <woody35545@gmail.com>
2023-08-26 18:41:47 +0900

    [~] #5  Modify ResponseObject extends org.springframework.http.ResponseEntity

--- src/main/java/com/teamseven/MusicVillain/Dto/ResponseBody/ResponseObject.java
+++ src/main/java/com/teamseven/MusicVillain/Dto/ResponseBody/ResponseObject.java
@@ -1,39 +1,147 @@
 package com.teamseven.MusicVillain.Dto.ResponseBody;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 
-public class ResponseObject {
+public class ResponseObject extends ResponseEntity {
+
+    /* ──────────────────────────── Fields ───────────────────────────── */
+
+    /* Private Fields */
+    private Status status; // -> This field does not need to be output to Response(JSON)
+
+    /* Public Fields */
     public int statusCode;
     public String message;
     public Object data;
+    /* ───────────────────────────────────────────────────────────────── */
 
-    public ResponseObject(int statusCode, String message, Object data){
-        this.statusCode = statusCode;
-        this.message = message;
-        this.data = data;
+    /* ──────────────────────── Generate Instance ────────────────────── */
+    public static ResponseObject of(Status status, String message, Object data){
+        // Customize message
+        return new ResponseObject(status, message, data);
+    }
+
+    public static ResponseObject of(Status status, Object data){
+        return new ResponseObject(status, data);
+    }
+    /* ─────────────────────────────────────────────────────────────── */
+
+    /* ──────────────────────── Constructors ──────────────────────── */
+    public ResponseObject(Status status){
+        super(HttpStatusCode.valueOf(status.getStatusCode()));
+        this.statusCode = status.getStatusCode();
+        this.message = status.getMessage();
+        this.data = null;
     }
 
     public ResponseObject(Status status, Object data){
+        super(data ,HttpStatusCode.valueOf(status.getStatusCode()));
         this.statusCode = status.getStatusCode();
         this.message = status.getMessage();
         this.data = data;
     }
-    public static ResponseObject of(Status status, String message, Object data){
-        return new ResponseObject(status.getStatusCode(), message, data);
+
+    // Customize message
+    public ResponseObject(Status status, String message, Object data){
+
+        super(data ,HttpStatusCode.valueOf(status.getStatusCode()));
+        this.statusCode = status.getStatusCode();
+        this.message = message;
+        this.data = data;
     }
-    public static ResponseObject of(Status status, Object data){
-        return new ResponseObject(status, data);
-    }
-    public static ResponseObject of(Status status){
-        return new ResponseObject(status, null);
+    /* ───────────────────────────────────────────────────────────────── */
+
+
+    /* ──────────────────────── For Convenience ──────────────────────── */
+    public static ResponseObject OK(Object data){
+        return new ResponseObject(Status.OK, data);
     }
 
-    public static ResponseObject authenticationFail(){
-        return new ResponseObject(Status.AUTHENTICATION_FAIL.getStatusCode(),
-                Status.AUTHENTICATION_FAIL.getMessage(), null);
+    public static ResponseObject BAD_REQUEST(Object data){
+        return new ResponseObject(Status.BAD_REQUEST);
     }
 
-    public static ResponseObject unauthorized(){
-        return new ResponseObject(Status.UNAUTHORIZED.getStatusCode(),
-                Status.UNAUTHORIZED.getMessage(), null);
+    public static ResponseObject NOT_FOUND(){
+        return new ResponseObject(Status.NOT_FOUND);
+    }
+
+    public static ResponseObject UNAUTHORIZED(){
+        return new ResponseObject(Status.UNAUTHORIZED);
+    }
+
+    public static ResponseObject CONFLICT(){
+        return new ResponseObject(Status.CONFLICT);
+    }
+
+    public static ResponseObject FORBIDDEN(){
+        return new ResponseObject(Status.FORBIDDEN);
+    }
+    /* ─────────────────────────────────────────────────────────── */
+
+
+    /* ──────────────────────── Override ──────────────────────── */
+
+
+    public ResponseObject(HttpStatusCode status) {
+        super(status);
+    }
+
+    public ResponseObject(Object body, HttpStatusCode status) {
+        super(body, status);
+    }
+
+    @Override
+    public HttpStatusCode getStatusCode() {
+        return super.getStatusCode();
+    }
+
+    @Override
+    public int getStatusCodeValue() {
+        return super.getStatusCodeValue();
+    }
+
+    @Override
+    public HttpHeaders getHeaders() {
+        return super.getHeaders();
+    }
+
+    @Override
+    public Object getBody() {
+        return super.getBody();
+    }
+
+    @Override
+    public boolean hasBody() {
+        return super.hasBody();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return super.equals(other);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public ResponseObject(Object body, MultiValueMap headers, int rawStatus) {
+        super(body, headers, rawStatus);
+    }
+
+    public ResponseObject(Object body, MultiValueMap headers, HttpStatusCode status) {
+        super(body, headers, status);
+    }
+
+    public ResponseObject(MultiValueMap headers, HttpStatusCode status) {
+        super(headers, status);
     }
 }
--- src/main/java/com/teamseven/MusicVillain/Member/MemberController.java
+++ src/main/java/com/teamseven/MusicVillain/Member/MemberController.java
@@ -1,6 +1,6 @@
 package com.teamseven.MusicVillain.Member;
 
-import com.teamseven.MusicVillain.Dto.ResponseBody.ResponseObject;
+import com.teamseven.MusicVillain.Dto.ResponseBody.ResponseObject_old;
 import com.teamseven.MusicVillain.Dto.RequestBody.MemberCreationRequestBody;
 import com.teamseven.MusicVillain.Security.JWT.MemberJwtAuthorizationManager;
 import com.teamseven.MusicVillain.Security.JWT.AuthorizationResult;
@@ -13,7 +13,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,30 +35,30 @@ public class MemberController {
 //    @Tag(name = "회원 관련 API", description = "Swagger 테스트")
     @GetMapping("/members")
     @Operation(summary = "모든 회원 조회", description = "데이터 베이스에 등록된 모든 회원 정보를 조회합니다.")
-    public ResponseObject members(){
+    public ResponseObject_old members(){
         log.debug("members() called - @GetMapping(\"/members\")");
         List<Member> memberList = memberService.getAllMembers();
-        return ResponseObject.of(Status.OK, memberList);
+        return ResponseObject_old.of(Status.OK, memberList);
     }
     @GetMapping("/members/{memberId}")
     @Operation(summary = "특정 회원 정보 조회", description = "특정 회원의 정보를 조회합니다.")
-    public ResponseObject getMemberById(
+    public ResponseObject_old getMemberById(
             @Parameter(description = "조회할 회원의 멤버 아이디", required = true)
             @PathVariable("memberId") String memberId){
         log.debug("getMemberById() called - @GetMapping(\"/members/{memberId}\")");
         log.debug("@PathVariable(\"memberId\"): {}", memberId);
         ServiceResult serviceResult = memberService.getMemberById(memberId);
-        return serviceResult.isFailed() ? ResponseObject.of(Status.BAD_REQUEST, null)
-                : ResponseObject.of(Status.OK, serviceResult.getData());
+        return serviceResult.isFailed() ? ResponseObject_old.of(Status.BAD_REQUEST, null)
+                : ResponseObject_old.of(Status.OK, serviceResult.getData());
     }
 
     @Hidden
     @PostMapping("/members")
-    public ResponseObject insertMember(@RequestBody MemberCreationRequestBody memberCreationRequestBody){
+    public ResponseObject_old insertMember(@RequestBody MemberCreationRequestBody memberCreationRequestBody){
         log.debug("insertMember() called - @PostMapping(\"/members\")");
         ServiceResult result = memberService.insertMember(memberCreationRequestBody);
-        return result.isFailed() ? ResponseObject.of(Status.CREATION_FAIL, result.getData())
-                : ResponseObject.of(Status.CREATED, result.getData());
+        return result.isFailed() ? ResponseObject_old.of(Status.CREATION_FAIL, result.getData())
+                : ResponseObject_old.of(Status.CREATED, result.getData());
     }
 
     /**
@@ -76,7 +75,7 @@ public class MemberController {
     @PostMapping("/members/{memberId}")
     @Operation(summary = "회원 이름 변경", description = "회원의 이름(닉네임)을 변경합니다.")
 
-    public ResponseObject modifyMemberNickname(
+    public ResponseObject_old modifyMemberNickname(
             @Parameter(description = "변경할 회원의 멤버 아이디", required = true)
             @PathVariable("memberId") String memberId,
             @Parameter(description = "변경할 닉네임", required = true)
@@ -87,12 +86,12 @@ public class MemberController {
         AuthorizationResult authResult =
                 authManager.authorize(requestHeader, memberId);
 
-        if (authResult.isFailed()) return ResponseObject.of(Status.UNAUTHORIZED, authResult.getMessage());
+        if (authResult.isFailed()) return ResponseObject_old.of(Status.UNAUTHORIZED, authResult.getMessage());
 
         ServiceResult result = memberService.modifyMemberNickname(memberId, nickname);
 
-        return result.isFailed() ? ResponseObject.of(Status.BAD_REQUEST, result.getData())
-                : ResponseObject.of(Status.OK, result.getData());
+        return result.isFailed() ? ResponseObject_old.of(Status.BAD_REQUEST, result.getData())
+                : ResponseObject_old.of(Status.OK, result.getData());
     }
 
     /**
@@ -107,7 +106,7 @@ public class MemberController {
     @DeleteMapping("/members/{memberId}")
     @Operation(summary = "회원 탈퇴", description = "회원을 탈퇴시킵니다.")
 
-    public ResponseObject deleteMemberByMemberId(
+    public ResponseObject_old deleteMemberByMemberId(
             @Parameter(description = "탈퇴할 회원의 멤버 아이디", required = true)
             @PathVariable("memberId") String memberId,
             @Parameter(description = "JWT Token", required = true)
@@ -117,8 +116,8 @@ public class MemberController {
 
         ServiceResult result = memberService.deleteMemberByMemberId(memberId);
 
-        return result.isFailed() ? ResponseObject.of(Status.BAD_REQUEST, result.getMessage())
-                : ResponseObject.of(Status.OK, result.getData());
+        return result.isFailed() ? ResponseObject_old.of(Status.BAD_REQUEST, result.getMessage())
+                : ResponseObject_old.of(Status.OK, result.getData());
     }
 
 }
