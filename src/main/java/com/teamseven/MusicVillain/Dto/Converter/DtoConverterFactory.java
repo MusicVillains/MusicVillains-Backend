b1252e9d8f9f360a40708436f3b6742bd49d33d0
kjeon0901 <kjeon0901@naver.com>
2023-08-26 23:13:52 +0900

    [+] Implement DtoConverterFactory and Refactor FeedDtoConverter + Add Converter interface to generalize DTO-Entity convert + Add DtoConverterFactory class for dynamic converter instance creation according to class type ~ Null checks for ownerId, ownerName, recordId ~ Mark as 'Component' to enable dependency injection

---
+++ src/main/java/com/teamseven/MusicVillain/Dto/Converter/DtoConverterFactory.java
@@ -0,0 +1,19 @@
+package com.teamseven.MusicVillain.Dto.Converter;
+
+import com.teamseven.MusicVillain.Dto.FeedDto;
+import com.teamseven.MusicVillain.Feed.Feed;
+
+public class DtoConverterFactory {
+    public <S, T> Converter<S, T> getConverter(Class<S> sourceClass, Class<T> targetClass) {
+
+        if (sourceClass == Feed.class && targetClass == FeedDto.class) {
+            return (Converter<S, T>) new FeedDtoConverter();
+//        } else if (sourceClass == Member.class && targetClass == MemberDto.class) {
+//            return (Converter<S, T>) new MemberDtoConverter();
+//        } else if (sourceClass == Notification.class && targetClass == NotificationDto.class) {
+//            return (Converter<S, T>) new NotificationDtoConverter();
+        } else {
+            throw new IllegalArgumentException("No suitable converter found.");
+        }
+    }
+}
