91ba071236eb4fc2ab88fabfe4432fb9c484b2ee
woody35545 <woody35545@gmail.com>
2023-08-27 17:52:10 +0900

    [~] Modify MemberService to use Dto

--- src/main/java/com/teamseven/MusicVillain/Dto/MemberDto.java
+++ src/main/java/com/teamseven/MusicVillain/Dto/MemberDto.java
@@ -1,15 +1,25 @@
 package com.teamseven.MusicVillain.Dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-public class MemberDto {
-    public String memberId; // PK, uuid.toString()
-    public String userId;
+@Builder
+@Data
+public class MemberDto implements DataTransferObject {
+    public final String memberId; // PK, uuid.toString(), Never change after initialized
+    public final String userId; // related to OAuth2 provider, Never change after initialized
+    @JsonIgnore
+    public String userInfo;
     public String name;
+    @JsonIgnore
     public String email;
     public String role;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
@@ -19,4 +29,14 @@ public class MemberDto {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     public LocalDateTime lastLoginAt;
     public String providerType;
+
+    @Override
+    public Object toDto(Object o) {
+        return null;
+    }
+
+    @Override
+    public List toDtoList(List list) {
+        return null;
+    }
 }
--- src/main/java/com/teamseven/MusicVillain/Feed/FeedService.java
+++ src/main/java/com/teamseven/MusicVillain/Feed/FeedService.java
@@ -1,9 +1,8 @@
 package com.teamseven.MusicVillain.Feed;
 
 import com.teamseven.MusicVillain.Dto.*;
-import com.teamseven.MusicVillain.Dto.Converter.Converter;
+import com.teamseven.MusicVillain.Dto.Converter.DtoConverter;
 import com.teamseven.MusicVillain.Dto.Converter.DtoConverterFactory;
-import com.teamseven.MusicVillain.Dto.Converter.FeedDtoConverter;
 import com.teamseven.MusicVillain.Interaction.Interaction;
 import com.teamseven.MusicVillain.Interaction.InteractionRepository;
 import com.teamseven.MusicVillain.Interaction.InteractionService;
@@ -49,12 +48,12 @@ public class FeedService {
         this.interactionService = interactionService;
     }
 
-    private Converter feedDtoConverter =
+    private DtoConverter feedDtoDtoConverter =
             DtoConverterFactory.getConverter(Feed.class, FeedDto.class);
 
     public ServiceResult getAllFeeds(){
         List<Feed> feeds = feedRepository.findAllByOrderByCreatedAtDesc();
-        List<FeedDto> feedDtoList = feedDtoConverter.convertList(feeds);
+        List<FeedDto> feedDtoList = feedDtoDtoConverter.convertToDtoList(feeds);
         return ServiceResult.success(feedDtoList);
     }
 
@@ -149,18 +148,20 @@ public class FeedService {
 
     public ServiceResult getAllFeedsByMemberId(String memberId) {
         List<Feed> feeds = feedRepository.findAllByOwnerMemberId(memberId);
-        List<FeedDto> resultFeedDtoList = feedDtoConverter.convertList(feeds);
+        List<FeedDto> resultFeedDtoList = feedDtoDtoConverter.convertToDtoList(feeds);
         return ServiceResult.success(resultFeedDtoList);
     }
 
     public ServiceResult getAllFeedsByFeedType(String feedType){
         List<Feed> feeds = feedRepository.findAllByFeedType(feedType);
-        List<FeedDto> resultFeedDtoList = feedDtoConverter.convertList(feeds);
+        List<FeedDto> resultFeedDtoList = feedDtoDtoConverter.convertToDtoList(feeds);
         return ServiceResult.success(resultFeedDtoList);
     }
 
     public ServiceResult feedViewCountUp(String feedId){
+
         Feed feed = feedRepository.findByFeedId(feedId);
+
         if(feed == null){
             return ServiceResult.of(ServiceResult.FAIL, "Feed Not Found");
         }
@@ -214,7 +215,7 @@ public class FeedService {
             interactionFeedList.add(interaction.getInteractionFeed());
         }
 
-        List<FeedDto> resultFeedDtoList = feedDtoConverter.convertList(interactionFeedList);
+        List<FeedDto> resultFeedDtoList = feedDtoDtoConverter.convertToDtoList(interactionFeedList);
         return ServiceResult.success(resultFeedDtoList);
 
     }
