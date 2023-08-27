6731d99be383af00c2d68f50bfea4df1482dc261
woody35545 <woody35545@gmail.com>
2023-08-27 22:02:27 +0900

    [+] Implement kakaoLogin api

--- src/main/java/com/teamseven/MusicVillain/Security/OAuth/OAuthController.java
+++ src/main/java/com/teamseven/MusicVillain/Security/OAuth/OAuthController.java
@@ -1,6 +1,10 @@
 package com.teamseven.MusicVillain.Security.OAuth;
 
+import com.teamseven.MusicVillain.Dto.ResponseBody.ResponseObject;
+import com.teamseven.MusicVillain.Dto.ResponseBody.Status;
+import com.teamseven.MusicVillain.Dto.ServiceResult;
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @CrossOrigin(origins = "*")
 @Controller
 public class OAuthController {
+
+    private OAuthService oAuthService;
+    @Autowired
+    public OAuthController(OAuthService oAuthService){
+        this.oAuthService = oAuthService;
+    }
 
     // OAuth2 로그인 성공시 토큰 생성해서 보내주는 부분
     @GetMapping("/OAuth/loginSuccess")
@@ -24,9 +34,19 @@ public class OAuthController {
         return "loginFailure";
     }
 
-    // [!] Implement later
+
+    // TODO: Test needed
     @PostMapping("/oauth2/kakao/login")
-    public String kakaoLogin(OauthLoginRequestBody oauthLoginRequestBody){
-        return "JWT Token";
+    public ResponseObject kakaoLogin(KakaoOAuthLoginRequestBody kakaoOAuthLoginRequestBody){
+
+        if(kakaoOAuthLoginRequestBody.code == null){
+            return ResponseObject.of(Status.BAD_REQUEST, "Authorization code is null", null);
+        }
+
+        ServiceResult kakaoOauthLoginResult = oAuthService.kakaoOauthLogin(kakaoOAuthLoginRequestBody.code);
+
+        return kakaoOauthLoginResult.isFailed() ? ResponseObject.of(
+                Status.AUTHENTICATION_FAIL, kakaoOauthLoginResult.getMessage(), null)
+                : ResponseObject.OK(kakaoOauthLoginResult.getData());
     }
 }
