a1c616428cebe527346f228b57f9f738868a95a8
woody35545 <woody35545@gmail.com>
2023-08-31 05:48:51 +0900

    [+] #19 Implement customResponseBodyMode in ResponseObject

--- src/main/java/com/teamseven/MusicVillain/Dto/ResponseBody/ResponseObject.java
+++ src/main/java/com/teamseven/MusicVillain/Dto/ResponseBody/ResponseObject.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
@@ -20,27 +19,53 @@ public class ResponseObject extends ResponseEntity {
      *  Even if there is no data to be transmitted,
      *  the body is forcibly put into Response to keep the format statusCode, message, and data
      *  using `CustomResponseBody` class
+     *
+     *  useCustomizedResponse 옵션을 사용하기 위해 기존 생성자를  private로 선언하여,
+     *  of 메소드로만 생성할 수 있도록 강제함.
      */
+
+    /* ──────────────────────── Option ────────────────────── */
+
+    /* WARN: test needed */
+    /* CUSTOM_RESPONSE_BODY_MODE
+       - if true: use customized response body(class CustomResponseBody),
+       - if false: use raw data as response body */
+    private static boolean customResponseBodyMode = false;
 
 
     /* ──────────────────────── Generate Instance ────────────────────── */
+    public static ResponseObject onlyData(Status status, Object data){
+        return new ResponseObject(data, status);
+    }
+
     public static ResponseObject of(Status status, String message, Object data){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(data, status);
+
         // Customize message
         return new ResponseObject(status, message, data);
     }
 
     public static ResponseObject of(Status status, Object data){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(data, status);
+
         return new ResponseObject(status, data);
     }
 
     public static ResponseObject of(Status status){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject("", status);
+
         return new ResponseObject(status);
     }
     /* ─────────────────────────────────────────────────────────────── */
 
     /* ──────────────────────── Constructors ──────────────────────── */
-    public ResponseObject(Status status){
-
+    private ResponseObject(Status status){
         super(CustomResponseBody.builder()
                 .statusCode(status.getStatusCode())
                 .message(status.getMessage())
@@ -48,7 +73,7 @@ public class ResponseObject extends ResponseEntity {
                 .build(), HttpStatusCode.valueOf(status.getStatusCode()));
     }
 
-    public ResponseObject(Status status, Object data){
+    private ResponseObject(Status status, Object data){
         super(CustomResponseBody.builder()
                 .statusCode(status.getStatusCode())
                 .message(status.getMessage())
@@ -57,8 +82,7 @@ public class ResponseObject extends ResponseEntity {
     }
 
     // Customize message
-    public ResponseObject(Status status, String message, Object data){
-
+    private ResponseObject(Status status, String message, Object data){
         super(CustomResponseBody.builder()
                 .statusCode(status.getStatusCode())
                 .message(message)
@@ -66,7 +90,10 @@ public class ResponseObject extends ResponseEntity {
                 .build() ,HttpStatusCode.valueOf(status.getStatusCode()));
     }
 
-    public ResponseObject(HttpHeaders headers, Status status, String message, Object data){
+    private ResponseObject(Object responseBody, Status status){
+        super(responseBody, HttpStatusCode.valueOf(status.getStatusCode()));
+    }
+    private ResponseObject(HttpHeaders headers, Status status, String message, Object data){
         super(CustomResponseBody.builder()
                 .statusCode(status.getStatusCode())
                 .message(message)
@@ -78,55 +105,94 @@ public class ResponseObject extends ResponseEntity {
 
     /* ──────────────────────── For Convenience ──────────────────────── */
     public static ResponseObject OK(Object data){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(data, Status.OK);
         return new ResponseObject(Status.OK, data);
     }
 
     public static ResponseObject NO_CONTENT(){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(Status.NO_CONTENT.getMessage(), Status.NO_CONTENT);
         return new ResponseObject(Status.NO_CONTENT);
     }
 
     public static ResponseObject BAD_REQUEST(){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(Status.BAD_REQUEST.getMessage(), Status.BAD_REQUEST);
         return new ResponseObject(Status.BAD_REQUEST);
     }
     public static ResponseObject BAD_REQUEST(Object data){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(data, Status.BAD_REQUEST);
         return new ResponseObject(Status.BAD_REQUEST, data);
     }
 
     public static ResponseObject NOT_FOUND(){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(Status.NOT_FOUND.getMessage(), Status.NOT_FOUND);
         return new ResponseObject(Status.NOT_FOUND);
     }
 
     public static ResponseObject UNAUTHORIZED(){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(Status.UNAUTHORIZED.getMessage(), Status.UNAUTHORIZED);
         return new ResponseObject(Status.UNAUTHORIZED);
     }
 
     public static ResponseObject UNAUTHORIZED(Object data){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(data, Status.UNAUTHORIZED);
+
         return new ResponseObject(Status.UNAUTHORIZED, data);
     }
 
     public static ResponseObject CONFLICT(){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(Status.CONFLICT.getMessage(), Status.CONFLICT);
         return new ResponseObject(Status.CONFLICT);
     }
 
     public static ResponseObject FORBIDDEN(){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(Status.FORBIDDEN.getMessage(), Status.FORBIDDEN);
         return new ResponseObject(Status.FORBIDDEN);
     }
 
 
     public static ResponseObject CREATED(){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(Status.CREATED.getMessage(), Status.CREATED);
         return new ResponseObject(Status.CREATED);
     }
     public static ResponseObject CREATED(Object data){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(data, Status.CREATED);
         return new ResponseObject(Status.CREATED, data);
     }
 
     public static ResponseObject CREATION_FAIL(){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(Status.CREATION_FAIL.getMessage(), Status.CREATION_FAIL);
         return new ResponseObject(Status.CREATION_FAIL);
     }
     public static ResponseObject CREATION_FAIL(Object data){
+        if(!customResponseBodyMode)
+            /* The message received as a parameter is ignored and raw data is entered in the body */
+            return new ResponseObject(data, Status.CREATION_FAIL);
         return new ResponseObject(Status.CREATION_FAIL, data);
     }
-
 
     /* ─────────────────────────────────────────────────────────── */
 
