bc96634e43d004acc6aed3dcc356a4717999d2b0
kjeon0901 <kjeon0901@naver.com>
2023-09-16 21:56:07 +0900

    [-] @Hidden 어노테이션 삭제

--- src/main/java/com/teamseven/MusicVillain/Record/RecordController.java
+++ src/main/java/com/teamseven/MusicVillain/Record/RecordController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@Hidden
 @RestController
 public class RecordController {
     private final RecordService recordService;
