a1fd72bc83aaf04885b761020ac90c182c4f6023
woody35545 <woody35545@gmail.com>
2023-09-18 16:11:22 +0900

    [+] Swagger에 JWT Auth를 위한 기능 추가

---
+++ src/main/java/com/teamseven/MusicVillain/Configuration/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package com.teamseven.MusicVillain.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+//@OpenAPIDefinition(
+//        info = @Info(title = "MusicVillains API",
+//                description = "refrence for MusicVillains API",
+//                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        Info info = new Info()
+                .version("v1")
+                .title("MusicVillains API")
+                .description("refrence for MusicVillains API");
+
+        // SecuritySecheme 이름
+        String jwtSchemeName = "Access Token(JWT)";
+
+        // API호출 시 전역으로 설정한 인증정보가 요청 헤더에 포함이 되도록 설정
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식으로 설정, HTTP 외에도 APIKEY, OAUTH2, OpenIdConnect 등 지원
+                        .scheme("bearer") // bearer token 방식을 사용
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+
+}
