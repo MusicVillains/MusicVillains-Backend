0765b9f22a8973153fab6d11dc67014de15cf213
woody35545 <woody35545@gmail.com>
2023-10-04 19:16:48 +0900

    [~] #77 ServiceResult 필드 초기화 메서드 추가

--- src/main/java/com/teamseven/MusicVillain/Dto/ServiceResult.java
+++ src/main/java/com/teamseven/MusicVillain/Dto/ServiceResult.java
@@ -11,28 +11,56 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class ServiceResult{
+
     public static final String SUCCESS = "success";
-    @Schema(example = "fail")
     public static final String FAIL = "fail";
 
     public String result;
     public String message;
     public Object data;
 
+    /**
+     * 외부에서 Constructor 사용할 수 없도록 private으로 선언
+     * 메서드를 통해 객체 생성하도록 강제
+     */
+
+    private ServiceResult(){
+
+    }
+    private ServiceResult(String result, String message, Object data){
+        this.result = result;
+        this.message = message;
+        this.data = data;
+    }
+
+
+    @Deprecated
     public static ServiceResult of(String result, String message, Object data){
         return new ServiceResult(result, message, data);
     }
 
+    @Deprecated
     public static ServiceResult of(String result, String message){
         return new ServiceResult(result, message, null);
     }
 
+    @Deprecated
     public static ServiceResult success(Object data){
         return new ServiceResult(SUCCESS, null, data);
     }
 
+    @Deprecated
     public static ServiceResult fail(String message){
         return new ServiceResult(FAIL, message, null);
+    }
+
+
+    public static ServiceResult SUCCESS(){
+        return new ServiceResult().success();
+    }
+
+    public static ServiceResult FAIL(){
+        return new ServiceResult().fail();
     }
 
     public boolean isSuccessful(){
@@ -41,6 +69,28 @@ public class ServiceResult{
 
     public boolean isFailed(){
         return this.result.equals(FAIL);
+    }
+
+    public ServiceResult success(){
+        this.result = SUCCESS;
+        this.message = SUCCESS;
+        return this;
+    }
+
+    public ServiceResult fail(){
+        this.result = FAIL;
+        this.message = FAIL;
+        return this;
+    }
+
+    public ServiceResult message(String message){
+        this.message = message;
+        return this;
+    }
+
+    public ServiceResult data(Object data){
+        this.data = data;
+        return this;
     }
 
 }
