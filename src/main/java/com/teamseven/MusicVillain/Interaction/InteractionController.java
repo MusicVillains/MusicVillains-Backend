d6fc0368cabf4c967f7a8f81c4e39379f7c02a0b
woody35545 <woody35545@gmail.com>
2023-10-15 14:38:11 +0900

    [+] #91 doInteraction 메서드에 인가처리 추가

--- src/main/java/com/teamseven/MusicVillain/Interaction/InteractionController.java
+++ src/main/java/com/teamseven/MusicVillain/Interaction/InteractionController.java
@@ -4,22 +4,32 @@ import com.teamseven.MusicVillain.Dto.RequestBody.InteractionCreationRequestBody
 import com.teamseven.MusicVillain.Dto.ResponseBody.ResponseObject;
 import com.teamseven.MusicVillain.Dto.ServiceResult;
 import com.teamseven.MusicVillain.Dto.ResponseBody.Status;
+import com.teamseven.MusicVillain.Security.JWT.AuthorizationResult;
+import com.teamseven.MusicVillain.Security.JWT.MemberJwtAuthorizationManager;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequiredArgsConstructor
 @Tag(name = "인터렉션 관련 API")
 public class InteractionController {
     
     private final InteractionService interactionService;
+
+    private MemberJwtAuthorizationManager memberAuthManager;
+
+    public InteractionController(InteractionService interactionService,
+                                 MemberJwtAuthorizationManager memberJwtAuthorizationManager) {
+        this.interactionService = interactionService;
+        this.memberAuthManager = memberJwtAuthorizationManager;
+    }
 
     /**
      * 상호작용 생성 | POST | /interactions
@@ -35,7 +45,18 @@ public class InteractionController {
      */
     @PostMapping("/interactions")
     @Operation(summary = "상호작용 생성", description = "상호작용을 생성합니다.")
-    public ResponseObject doInteraction(@RequestBody InteractionCreationRequestBody requestBody){
+    public ResponseObject doInteraction(
+            @RequestBody InteractionCreationRequestBody requestBody,
+            @RequestHeader HttpHeaders headers
+    ){
+
+        AuthorizationResult authResult
+                = memberAuthManager.authorize(headers,requestBody.getMemberId());
+
+        if(authResult.isFailed()){
+            return ResponseObject.UNAUTHORIZED(authResult.getMessage());
+        }
+
         ServiceResult result =  interactionService.insertInteraction(requestBody);
         return result.isFailed() ? ResponseObject.BAD_REQUEST(result.getMessage())
                 : ResponseObject.OK(result.getMessage() + " - " + result.getData());
