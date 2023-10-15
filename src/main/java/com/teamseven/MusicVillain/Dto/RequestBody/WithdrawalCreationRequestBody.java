37390071c2bf13d083daf8faf631aed658884dc4
kjeon0901 <kjeon0901@naver.com>
2023-10-15 20:08:49 +0900

    [+] deleteMemberByMemberId()에서 탈퇴원 정보 Withdrawal DB에 담는 과정 추가

--- src/main/java/com/teamseven/MusicVillain/Dto/RequestBody/WithdrawalCreationRequestBody.java
+++ src/main/java/com/teamseven/MusicVillain/Dto/RequestBody/WithdrawalCreationRequestBody.java
@@ -1,6 +1,9 @@
 package com.teamseven.MusicVillain.Dto.RequestBody;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Data
 public class WithdrawalCreationRequestBody {
